5a3f94703ec671d0184743019d912913e6dd196e
KapyBoiveau <q.boiveau@gmail.com>
2023-09-22 14:37:55 +0200

    day 12

--- 2018/day12/d12p2.py
+++ 2018/day12/d12p2.py
@@ -7,11 +7,10 @@ for r in instr[2:]:
     r = r.split()
     rules[r[0]] = r[2]
 
-for _ in range(20):
-    newState = ''.join([rules['.'*i + state[:5-i]] for i in range(3,0,-1)])
-    for i in range(2,len(state)-2):
-        newState += rules[state[i-2:i+3]]
-    newState += ''.join([rules[state[-(5-i):] + '.'*i] for i in range(1,4)])
+for _ in range(100):
+    newState = ''.join([rules['.'*i + state[:5-i]] for i in range(3,0,-1)]) # extend on the left
+    newState += ''.join([rules[state[i-2:i+3]] for i in range(2,len(state)-2)]) # every pot except the ones at the start & end
+    newState += ''.join([rules[state[-(5-i):] + '.'*i] for i in range(1,4)]) # extend on the right
     state = newState
 
-print(sum([i-20 for i in range(len(state)) if state[i]=='#']))
+print(sum([i-100 for i in range(len(state)) if state[i]=='#']) + (50000000000 - 100) * 80)
