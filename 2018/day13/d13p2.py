cc87dd4ba823a24f7111a0e1f2430c4d9c1ab76c
KapyBoiveau <q.boiveau@gmail.com>
2023-09-26 17:05:29 +0200

    13 p2 with better sort

--- 2018/day13/d13p2.py
+++ 2018/day13/d13p2.py
@@ -7,23 +7,9 @@ for y,line in enumerate(t.space):
         if tile in ('>','v','^','<'):
             carList.append(w.car(x,y,tile))
 
-def sort_the_cars():
-    vals = []
-    carList2 = []
-    global carList
-    for c in carList:
-        vals.append(c.pos[1]*1000 + c.pos[0])
-    vals.sort()
-    for v in vals:
-        for c in carList:
-            if c.pos[1]*1000 + c.pos[0] == v:
-                carList2.append(c)
-                break
-    carList = carList2
-
 finish = False
 while not finish:
-    sort_the_cars()
+    carList.sort(key=lambda car: (car.pos[1], car.pos[0]))
     for i,car in enumerate(carList):
         if not car.crashed:
             car.move(t)
