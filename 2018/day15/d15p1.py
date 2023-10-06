5b4348cc78bea3d11716bda21cdac2c4d1f94d0c
KapyBoiveau <q.boiveau@gmail.com>
2023-10-06 16:36:55 +0200

    day 15 en cours

--- 2018/day15/d15p1.py
+++ 2018/day15/d15p1.py
@@ -1,20 +1,62 @@
-import re
+import numpy as np
 
-nbPos = []
-start = []
-for i,l in enumerate(open('./2016/day15/input.txt', 'r').readlines()):
-    s = re.search(r'(\d+) p.+n (\d+)', l)
-    nbPos.append(int(s.groups()[0]))
-    start.append(int(s.groups()[1]))
+class unit:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.hp = 300
+        self.race = map[y][x]
 
-time = -1
-while(True):
-    time += 1
-    nope = False
-    for i in range(len(start)):
-        if (start[i]+time+1+i) % nbPos[i] != 0:
-            nope = True
-            break
-    if not nope:
-        break
-print(time)
+    def inRange(self):
+        pos = np.array([self.x, self.y])
+        return set(tuple(pos+dir) for dir in [(0,-1),(-1,0),(1,0),(0,1)] if map[tuple(pos+dir)[::-1]] == '.')
+
+    def victime(self):
+        self.hp -= 3
+
+    def moveTo(self, pos):
+        self.x = self.x
+        self.y = self.y
+
+map = list()
+for line in open('./2018/day15/test.txt', 'r').readlines():
+    map.append(list(line.strip()))
+
+map = np.array(map)
+unitList = list()
+unitPos = np.where(np.logical_or(map == 'G', map == 'E'))
+for i in range(len(unitPos[0])): 
+    unitList.append(unit(unitPos[1][i], unitPos[0][i]))
+
+def distance(pos1, pos2):
+    return 0
+
+def getClosest(range, u):
+    minDist = 10000
+    for r in range:
+        dist = distance(r, (u.x, u.y))
+        if dist < minDist:
+            minDist = dist
+            minPos = [r]
+        elif dist == minDist:
+            minPos.append(r)
+    minPos.sort(key=lambda u: (u.y, u.x))
+    return minPos[0]
+
+def getUnit(pos):
+    for u in unitList:
+        if (u.x, u.y) == pos:
+            return u
+
+# while True:
+unitList.sort(key=lambda u: (u.y, u.x))
+for i in range(len(unitList)): # for each unit
+    u = unitList[i]
+    rangeSet = set()
+    for u2 in unitList[:i]+unitList[i+1:]:
+        rangeSet = rangeSet.union(u2.inRange())
+    posVictime = getClosest(rangeSet, u)
+    if distance((u.x, u.y), posVictime) == 1:
+        getUnit(posVictime).victime()
+    else:
+        u.moveTo(posVictime)
