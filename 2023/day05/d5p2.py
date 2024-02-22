033c17a0b183cf60edab13f1a2d1703af38d960d
KapyBoiveau <q.boiveau@gmail.com>
2024-02-22 10:28:03 +0100

    5

--- 2023/day05/d5p2.py
+++ 2023/day05/d5p2.py
@@ -2,7 +2,7 @@
 mapping = dict()
 input = open('./2023/day05/input.txt', 'r').readlines()
 seeds = input[0].split()[1:]
-seeds = [[(int(seeds[i]), int(seeds[i])+int(seeds[i+1]))] for i in range(len(seeds)) if i%2 == 0]
+seeds = [(int(seeds[i]), int(seeds[i]) + int(seeds[i+1]) - 1) for i in range(len(seeds)) if i%2 == 0]
 for line in input[2:]:
     if len(line) > 1:
         if line[0].isdigit():
@@ -13,22 +13,36 @@ for line in input[2:]:
 
 def intersec(r1, r2):
     if not(r1[1] < r2[0] or r2[1] < r1[0]): # not no overlap
-        return (max((r1[0], r2[0])), min((r1[1], r2[1])))
+        overlap = (max((r1[0], r2[0])), min((r1[1], r2[1])))
+        remain = set()
+        if r1[0] < r2[0]:
+            remain.add((r1[0], r2[0] - 1))
+        if r1[1] > r2[1]:
+            remain.add((r2[1] + 1, r1[1]))
+        return overlap, remain
+    else:
+        return None, {r1} # no overlap, and remain = range1
 
-mini = 9999999999
-modes = list(mapping.keys())
-for s in seeds: # pour chaque range de seed
-    for mode in modes: # pour chaque mode
-        sTemps = list()
-        for subS in s: # pour chaque bloc de range, si jamais c'est split
-            for m in mapping[mode]: 
-                overlap = intersec(subS , (m[1], m[1] + m[2])) # on trouve l'intersection des ranges
+minis = []
+for seed in seeds: # pour chaque range de seed
+    seed = {seed}
+    for mode in mapping.keys(): # pour chaque mode
+        nextSeeds = set()
+        for m in mapping[mode]: 
+            rest = list()
+            
+            for s in seed:
+                overlap, s = intersec(s , (m[1], m[1] + m[2] - 1)) # on trouve l'intersection des ranges
                 if overlap:
-                    sTemps.append(destination(overlap)) # on met l'overlap dans une liste temporaire
-                    subS -= overlap # on enlève cet overlap et on continue avec les autres ranges du mode
-            if subS:
-                sTemps.append(subS) # on rajoute dans la liste ce qu'il reste
-        s = sTemps # la liste de nouvelles ranges devient la nouvelle "seed"
-    if min(s) < mini:
-        mini = min(s)
-print(mini)
+                    dest = (m[0] - m[1] + overlap[0], m[0] - m[1] + overlap[1]) # la destination correspondant à l'overlap
+                    nextSeeds.add(dest) # on met ça dans une liste temporaire
+                for subR in s:
+                    rest.append(subR)
+            seed = set(rest)
+
+        # on rajoute dans la liste ce qui reste
+        seed = nextSeeds.union(seed) # la liste de nouvelles ranges devient la nouvelle seed pour les mods suivants
+    
+    minis.append(sorted(list(seed))[0][0])
+
+print(min(minis))
