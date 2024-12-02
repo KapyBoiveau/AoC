38c6053398021c1bc7a72a5209fe8abb94998f1e
KapyBoiveau <q.boiveau@gmail.com>
2024-12-02 17:54:07 +0100

    v2 because I discovered np.diff

---
+++ 2024/day02/day02_p2_v2.py
@@ -0,0 +1,15 @@
+import re
+import numpy as np
+
+def isSafe(report):
+    gaps = np.diff(report)
+    return all(1 <= gap <= 3 for gap in gaps) or all(-3 <= gap <= -1 for gap in gaps)
+
+input = open('./2024/day02/input.txt', 'r').read().split('\n')
+
+safes = 0
+for line in input:
+    report = list(map(int, line.split()))
+    safes += any(isSafe(report[:x] + report[x+1:]) for x in range(len(report)))
+
+print(safes)
