5702d005fdbec6b138e40c4a4c2bf1eee0e55b7e
KapyBoiveau <q.boiveau@gmail.com>
2024-12-02 17:13:56 +0100

    cleaned a little

--- 2024/day02/day02_p2.py
+++ 2024/day02/day02_p2.py
@@ -4,18 +4,13 @@ reports = [list(map(int, line.split())) for line in input]
 
 safes = 0
 for report in reports:
-    inc, dec = 0, 0
-    for i in range(len(report)-1):
-        inc += report[i] < report[i+1]
-        dec += report[i] > report[i+1]
-
     for x in range(len(report)):
-        r = report[:x] + report[x+1:] # check without one element
-        if dec > inc: # mainly decreasing
+        r = report[:x] + report[x+1:] # remove indice x
+        if r[0] > r[1]: # decreasing
             if all(1 <= r[i]-r[i+1] <= 3 for i in range(len(r)-1)):
                 safes += 1
                 break
-        else:
+        else: # increasing
             if all(1 <= r[i+1]-r[i] <= 3 for i in range(len(r)-1)):
                 safes += 1
                 break
