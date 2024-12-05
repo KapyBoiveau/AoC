254fc575189c07f3a2c0f9efcf3c31f9edfd1d4d
KapyBoiveau <q.boiveau@gmail.com>
2024-12-05 11:56:54 +0100

    minor adjustments

--- 2024/day01/day01_p2.py
+++ 2024/day01/day01_p2.py
@@ -2,6 +2,7 @@ import re
 
 input = open('./2024/day01/input.txt', 'r').read()
 numbers = list(map(int, re.findall(r'\d+', input)))
-left, right = numbers[::2], numbers[1::2]
+left = numbers[::2]
+right = numbers[1::2]
 
 print(sum([x * right.count(x) for x in left]))
