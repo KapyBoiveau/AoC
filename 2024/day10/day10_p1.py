6b6ecf0998fc903cb43949622b697486c7e9e2b9
KapyBoiveau <q.boiveau@gmail.com>
2024-12-11 15:54:06 +0100

    cleaner d10

--- 2024/day10/day10_p1.py
+++ 2024/day10/day10_p1.py
@@ -3,18 +3,15 @@ import numpy as np
 grid = np.array([list(x) for x in open('./2024/day10/input.txt', 'r').read().split('\n')], dtype=int)
 grid = np.pad(grid, 1, constant_values=-1)
 
-def around(pos):
+def around(pos, val):
     y, x = pos
-    return [(y-1,x), (y,x-1), (y,x+1), (y+1,x)]
+    posAround = [(y-1, x), (y, x-1), (y, x+1), (y+1, x)]
+    return [(ny, nx) for ny, nx in posAround if grid[ny, nx] == val + 1]
 
 def walk(pos):
     y, x = pos
-    nbFinish = set()
     if grid[y,x] == 9:
         return {(y,x)}
-    for a in around(pos):
-        if grid[a] == grid[y,x] + 1:
-            nbFinish.update(walk(a))
-    return nbFinish
+    return set().union(*(walk(a) for a in around(pos, grid[y,x])))
     
 print(sum(len(walk(pos)) for pos in np.argwhere(grid == 0)))
