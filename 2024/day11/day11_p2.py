0289074b90524c4f02d0253a75ace05b3c835054
KapyBoiveau <q.boiveau@gmail.com>
2024-12-11 23:03:29 +0100

    d11 to change

--- 2024/day11/day11_p2.py
+++ 2024/day11/day11_p2.py
@@ -1,6 +1,10 @@
 import numpy as np
 import math
 
+def split_number(n, nbDigits):
+    half = 10 ** (nbDigits // 2)  # Puissance de 10 pour séparer
+    return n // half, n % half # left, right
+
 def blink(key):
     stone, n = key
     global dico
@@ -12,13 +16,12 @@ def blink(key):
         else:
             nbDigits = int(math.log10(stone)) + 1
             if nbDigits % 2 == 0:
-                half = 10 ** (nbDigits // 2)  # puissance de 10
-                left, right = stone // nbDigits, stone % nbDigits
+                left, right = split_number(stone, nbDigits)
                 dico[key] = blink((left, n+1)) + blink((right, n+1))
             else:
                 dico[key] = blink((stone * 2024, n+1))
     return dico[key]
-
+    
 dico = {}   
 stones = np.array(open('./2024/day11/input.txt', 'r').read().split(), dtype=int)
 print(sum(blink((stone, 0)) for stone in stones))
